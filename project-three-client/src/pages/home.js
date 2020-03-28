cadb882bf2ce8235fbc2933d0b5f37d605f2bcdf
Ben Taylor <bentaylor@knights.ucf.edu>
2020-03-27 21:46:55 -0400

    set up home page visually added imports for future use

--- project-three-client/src/pages/home.js
+++ project-three-client/src/pages/home.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
 
-export class home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
-        )
-    }
+import Toot from '../components/toot/Toot';
+import Profile from '../components/profile/Profile';
+import TootSkeleton from '../util/TootSkeleton';
+
+import { connect } from 'react-redux';
+import { getToots } from '../redux/actions/dataActions';
+
+class home extends Component {
+  componentDidMount() {
+    this.props.getToots();
+  }
+  render() {
+    const { tootss, loading } = this.props.data;
+    let recentTootsMarkup = !loading ? (
+      toots.map((toot) => <Toot key={toot.tootId} toot={toot} />)
+    ) : (
+      <TootSkeleton />
+    );
+    return (
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {recentTootsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <Profile />
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default home
+home.propTypes = {
+  getToots: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  data: state.data
+});
+
+export default connect(
+  mapStateToProps,
+  { getToots }
+)(home);
